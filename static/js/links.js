d3355257189b7d1a52a4a2c3669b878dc28e3795
vladibuyanov <vladibuyanov@gmail.com>
2023-01-04 11:26:49 +0100

    04/01/2023
    1. Form for group on main page - was closed
    2. Working at design
    3. Minor fixed

--- static/js/links.js
+++ static/js/links.js
@@ -6,7 +6,7 @@ for (let button_number = 0; button_number < scroll_buttons.length; button_number
     scroll_button.addEventListener('click', function scroll() {
         scroll_to[button_number].scrollIntoView({
             behavior: "smooth",
-            block: "start"
+            block: "center"
         });
     });
 }
--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,11 @@
 let scroll_button = document.getElementById('main-page-btn');
-let scroll_to = document.getElementById('contact-us-title');
+let scroll_to = document.getElementById('who-we-are-content');
 
 scroll_button = scroll_button.addEventListener('click', scroll)
 
 function scroll() {
     scroll_to.scrollIntoView({
             behavior: "smooth",
-            block: "start"
+            block: "center"
         });
 }
