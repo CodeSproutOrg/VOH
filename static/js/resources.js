9edcaf459cfb7d98b208996f4bad98a07c5b8726
Vladi <vladibuyanov@gmail.com>
2023-02-06 12:58:57 +0100

    26.01.2023
    1. Added apps page
    2. Minor design changes

--- static/js/resources.js
+++ static/js/resources.js
@@ -1,23 +1,29 @@
 let link_button = document.getElementById('link-btn');
 let video_btn = document.getElementById('video-btn');
 let document_btn = document.getElementById('document-btn');
+let app_btn = document.getElementById('app-btn');
+
+console.log(app_btn)
 
 link_button = link_button.addEventListener('click', open_link.bind(null,1));
 video_btn = video_btn.addEventListener('click', open_link.bind(null,2));
 document_btn = document_btn.addEventListener('click', open_link.bind(null,3));
+app_btn = app_btn.addEventListener('click', open_link.bind(null,4));
 
 function open_link(number) {
     let links = document.getElementById('links');
     let videos = document.getElementById('videos');
     let documents = document.getElementById('documents');
+    let apps = document.getElementById('apps');
 
     switch (number) {
         case 1:
-            links.classList.add('active');
-
             videos.classList.remove('active');
             videos.style.padding = '0';
             documents.classList.remove('active');
+            apps.classList.remove('active');
+
+            links.classList.add('active');
             break
         case 2:
             videos.classList.add('active');
@@ -25,6 +31,7 @@ function open_link(number) {
 
             links.classList.remove('active');
             documents.classList.remove('active');
+            apps.classList.remove('active');
             break
         case 3:
             documents.classList.add('active');
@@ -32,6 +39,17 @@ function open_link(number) {
             links.classList.remove('active');
             videos.classList.remove('active');
             videos.style.padding = '0';
+            apps.classList.remove('active');
+            break
+
+        case 4:
+            apps.classList.add('active');
+
+            links.classList.remove('active');
+            documents.classList.remove('active');
+            videos.classList.remove('active');
+            videos.style.padding = '0';
+
             break
     }
 }
